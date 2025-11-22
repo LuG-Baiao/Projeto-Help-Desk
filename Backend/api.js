928b6c19493d51ad3f3a85c52fe962df5d09fab1
MatheusS2027 <ms765370@gmail.com>
2025-11-22 16:36:36 -0300

    att

--- Backend/api.js
+++ Backend/api.js
@@ -6,10 +6,22 @@
 //Put / pacth
 //Delete
 
-import express from 'express';
-
+//import express from 'express';
+const express = require('express');
 const app = express();
 const port = 3000;  
+
+app.get("/",function(req,res){
+  res.send("Tela Login") //send é o q será enviado para o front end
+});
+
+app.get("/Home",function(req,res){
+  res.send("Direcionamento para tela principal")
+});
+
+app.get("/cadastro_cliente",function(req,res){
+  res.send("Cadastrar Clientes")
+});
 
 app.listen(port, () => console.log(`Server running at port ${port}`));
 
--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,21 +1,22 @@
-import { Sequelize } from "sequelize";
+const {Sequelize}= require("sequelize");
 
 // db/connection.js
 const sequelize = new Sequelize( 
-    "projeto_inclusão_social",
-    "Root",
-    "Luc@s2002",
+    "db_user_1",
+    "user_1",
+    "Twq7e9YE-1!",
     {
-        host: "localhost",
+        host: "phpmyadmin.uni9.marize.us",
         dialect: "mysql",
     }
 );
 
-sequelize.authenticate().theh((function(){
+sequelize.authenticate().then((function(){
     console.log("Conexão com o banco de dados realizada com sucesso.")
 })).catch((function(erro){
     console.log("Não foi possível conectar ao banco de dados: "+ erro)
 }));
+
 module.exports = {
     Sequelize: Sequelize,
     sequelize: sequelize
